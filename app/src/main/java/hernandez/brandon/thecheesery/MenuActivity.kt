64a814cd6c8d56584987c039919626dcca5680bc
Brandon Hernandez <56750116+MechanicalPuppet@users.noreply.github.com>
2021-02-19 17:43:29 -0600

    Repositorio inicializado

---
+++ app/src/main/java/hernandez/brandon/thecheesery/MenuActivity.kt
@@ -0,0 +1,15 @@
+package hernandez.brandon.thecheesery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+    }
+}
